651cfced58f1335e303afb52c6bf1747ec8e5b13
Radzhabov Rustam <rrradzhabov@edu.hse.ru>
2024-03-30 12:04:13 +0300

    Add two boxes: AI and playerbox

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.tictactoe
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tictactoe.ui.theme.TicTacToeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +36,53 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TTTScreen()
                 }
             }
         }
     }
 }
 
+@SuppressLint("RememberReturnType")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TTTScreen() {
+    // true - player's turn, false - AI's turn
+    val playerTurn = remember{ mutableStateOf(true)}
+
+    val moves = remember {mutableStateListOf<Boolean?>(true, null, false, null, true, false, null, null, null) }
+
+    Header(playerTurn.value)
+    Board(moves)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TicTacToeTheme {
-        Greeting("Android")
+fun Header(playerTurn: Boolean) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        val playerBoxColor = if (playerTurn) Color.Blue else Color.LightGray
+        val aiBoxColor = if (playerTurn) Color.LightGray else Color.Red
+
+        Box(modifier = Modifier
+            .width(100.dp)
+            .background(playerBoxColor))
+        {
+            Text(text = "Player", modifier = Modifier
+                                            .padding(8.dp)
+                                            .align(Alignment.Center))
+
+        }
+        Spacer(modifier = Modifier.width(50.dp))
+
+        Box(modifier = Modifier.width(100.dp).background(aiBoxColor)) {
+            Text(text = "AI", modifier = Modifier.padding(8.dp).align(Alignment.Center))
+        }
+
     }
+}
+
+@Composable
+fun Board(moves: List<Boolean?>) {
+
 }
